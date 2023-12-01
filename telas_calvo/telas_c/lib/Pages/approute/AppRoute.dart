65e70f64056717858c6ccdf75dae3548e79f4425
William Wallace <bostawwtr@outlook.com>
2023-12-01 03:29:39 -0300

    feat: list user pets

--- telas_calvo/telas_c/lib/Pages/approute/AppRoute.dart
+++
@@ -1,9 +0,0 @@
-class Routaaas {
-  static String Animal_Cadastro = "/cadastro_pet";
-  static String Animal_adicionar = "/cadastro_adicionar";
-  static String Home = "/Home";
-  static String Cadastro_user = "/cadastrio_user";
-  static String Login = "/login";
-  static String Animal_editar = "/Animal_editar_";
-  static String Profile = "/profile";
-}
