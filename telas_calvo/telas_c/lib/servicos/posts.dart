a60bdd566feeec47f89719d2053062ba1d1477e8
Gabriel Lechado <gabrielmilanz00@gmail.com>
2023-12-03 18:44:48 -0300

    cu

--- telas_calvo/telas_c/lib/servicos/posts.dart
+++ telas_calvo/telas_c/lib/servicos/posts.dart
@@ -2,10 +2,17 @@ import 'dart:convert';
 import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
+import 'package:telas_c/componentes/Postmodel.dart';
 import 'package:telas_c/Pages/approute/AppRoute.dart';
 import 'package:telas_c/Pages/home.dart';
 import 'package:telas_c/servicos/dados_autenticados.dart';
+
+  Future<void> _postListFromSnapshot() async{
+      final response = await http.post(
+      Uri.parse('http://localhost:3333/posts')
+
+    );
+  }
 
 Future<void> CreatePost(String idpet, String id) async {
     final response = await http.post(
