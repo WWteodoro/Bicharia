54420cd784fa44d0c8383fbc1b1992536c309ea8
Gabriel Lechado <gabrielmilanz00@gmail.com>
2023-12-10 17:28:14 -0300

    Fix: posts.dart

--- telas_calvo/telas_c/lib/servicos/posts.dart
+++ telas_calvo/telas_c/lib/servicos/posts.dart
@@ -3,19 +3,19 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
-import 'package:telas_c/Pages/approute/AppRoute.dart';
-import 'package:telas_c/Pages/home.dart';
-import 'package:telas_c/servicos/dados_autenticados.dart';
+import 'package:telas_c/componentes/Postmodel.dart';
 
-Future<void> CreatePost(String idpet, String id, String text) async {
-    final response = await http.post(
+
+Future<void> CreatePost(String photo, String userId, String text) async {
+    final post = await http.post(
       Uri.parse('http://localhost:3333/posts'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8'
       },
-      body: jsonEncode(<String, String>{
-          'Id Pet' : idpet,
-          'User Id' : id,
+      body: jsonEncode(<String, dynamic>{
+        "photo": photo,
+        "userId": userId,
+        "text": text,
       })
     );
   }
