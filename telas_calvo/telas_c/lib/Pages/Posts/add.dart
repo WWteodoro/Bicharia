adeb5ebfe72d12316747b1aef26f588277d66735
Gabriel Lechado <gabrielmilanz00@gmail.com>
2023-12-10 15:12:10 -0300

    Telas de add posts
    Testa calvo

--- telas_calvo/telas_c/lib/Pages/Posts/add.dart
+++ telas_calvo/telas_c/lib/Pages/Posts/add.dart
@@ -14,12 +14,14 @@ class Add extends StatefulWidget {
 
 class _AddState extends State<Add> {
   String text = '';
+  late String urlImage;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
     appBar: AppBar(
       title: Text('Add Post'),
       actions: <Widget>[
+<<<<<<< Updated upstream:telas_calvo/telas_c/lib/Pages/Posts/add.dart
         TextButton(
           style: TextButton.styleFrom(foregroundColor: Colors.white,),
           onPressed:()async{
@@ -27,6 +29,22 @@ class _AddState extends State<Add> {
           }
           ,
           child: Text('Post'))
+=======
+        IconButton(
+          onPressed: () async{
+            urlImage = (await pickImage())!;
+          },
+          icon:Icon(Icons.upload),
+          color: Colors.orange,
+        ),
+        IconButton(
+          onPressed: () {
+            CreatePost(urlImage, Dados_Usuario.id, text);
+          },
+          icon: Icon(Icons.add),
+          color: Colors.orange,
+        ),
+>>>>>>> Stashed changes:telas_calvo/telas_c/lib/Pages/add.dart
       ],
     ),
     body: Container(
